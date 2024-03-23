5d1596cc36874bfa57c0ffe861d894e34f4af9db
MrKoBo <kdtate4@memphis.edu>
2024-03-23 14:06:48 -0500

    Added initialize method to deck and shuffle method

--- lib/midterm_project.rb
+++ lib/midterm_project.rb
@@ -1,22 +1,6 @@
 #lib/midterm_project.rb
 
 class Card
-  possible_suits = ["Diamonds", "Hearts", "Clubs", "Spades"]
-  possible_values = {
-  "Ace" => [1, 14],
-  "Two" => 2,
-  "Three" => 3,
-  "Four" => 4,
-  "Five" => 5,
-  "Six" => 6,
-  "Seven" => 7,
-  "Eight" => 8,
-  "Nine" => 9,
-  "Ten" => 10,
-  "Jack" => 11,
-  "Queen" => 12,
-  "King" => 13
-  }
   attr_accessor :suit, :value
   def initialize(value, suit)
     @suit = suit
@@ -28,6 +12,40 @@ class Card
   end
 end
 class Deck
+  def initialize
+    @suits = ["Diamonds", "Hearts", "Clubs", "Spades"]
+    @values = {
+      "Ace" => [1, 14],
+      "Two" => 2,
+      "Three" => 3,
+      "Four" => 4,
+      "Five" => 5,
+      "Six" => 6,
+      "Seven" => 7,
+      "Eight" => 8,
+      "Nine" => 9,
+      "Ten" => 10,
+      "Jack" => 11,
+      "Queen" => 12,
+      "King" => 13
+    }
+    @start_deck = []
+    @values.each do |key, value|
+      @suits.each do |item|
+        card = Card.new(key, item)
+        @start_deck << card
+      end
+    end
+    @active_deck = @start_deck
+  end
+
+  def shuffle_deck
+    @active_deck.shuffle!
+    @active_deck.each do |item|
+      item.display
+    end
+
+  end
 end
 class Hnad
 end
