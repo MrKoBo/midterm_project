5503628f2ab6024a9299cbfc2145c78df75bf214
MrKoBo <kdtate4@memphis.edu>
2024-03-28 23:09:08 -0500

    Made all strength test go green

--- new_midterm_project/lib/new_midterm_project.rb
+++ new_midterm_project/lib/new_midterm_project.rb
@@ -54,7 +54,7 @@ class Deck
 end
 
 class Hand
-  attr_accessor :active_hand, :sorted_values
+  attr_accessor :active_hand
 
   def initialize(active_hand)
     @active_hand = active_hand
@@ -71,22 +71,75 @@ class Hand
       "High Card" => 1
     }
     @current_values = []
+    @hand_suits = []
     @active_hand.each do |card|
       @current_values << card.value
+      @hand_suits << card.suit
     end
     @sorted_values = @current_values.sort
   end
 
   def strength
+    return "Royal Flush" if royal_flush
+    return "Straight Flush" if straight_flush
+    return "Four of a Kind" if four_of_a_kind
+    return "Full House" if full_house
+    return "Flush" if flush
     return "Straight" if straight
+    return "Three of a Kind" if three_of_a_kind
+    return "Two Pairs" if two_pairs
+    return "Pair" if pair
     "High Card"
   end
 
-  private
 
+  def royal_flush
+    first_suit = @hand_suits[0]
+    @hand_suits.each do |suit|
+      return false if suit != first_suit
+    end
+    return false if @sorted_values != [1, 10, 11, 12, 13]
+    true
+  end
+  def straight_flush
+    first_suit = @hand_suits[0]
+    @hand_suits.each do |suit|
+      return false if suit != first_suit
+    end
+    (0...4).each do |i|
+      return false unless @sorted_values[i] + 1 == @sorted_values[i + 1]
+    end
+    return true
+
+  end
+  def four_of_a_kind
+    counts = Hash.new(0)
+    @sorted_values.each do |item|
+      counts[item] += 1
+    end
+    counts.each do |key, value|
+      return true if counts[key] == 4
+    end
+    false
+  end
+  def full_house
+    counts = Hash.new(0)
+    @sorted_values.each do |item|
+      counts[item] += 1
+    end
+    counts.each do |key, value|
+      return true if (counts[key] == 3 && counts.length == 2)
+    end
+    false
+  end
+  def flush
+    first_suit = @hand_suits[0]
+    @hand_suits.each do |suit|
+      return false if suit != first_suit
+    end
+    true
+  end
   def straight
-    return false if @sorted_values.nil? || @sorted_values.empty?
-    # Check for a regular straight (non-wrap-around)
     return true if @sorted_values == [1, 10, 11, 12, 13]
     (0...4).each do |i|
       return false unless @sorted_values[i] + 1 == @sorted_values[i + 1]
@@ -94,12 +147,36 @@ class Hand
 
     return true
   end
+  def three_of_a_kind
+    counts = Hash.new(0)
+    @sorted_values.each do |item|
+      counts[item] += 1
+    end
+    counts.each do |key, value|
+      return true if counts[key] == 3
+    end
+    false
+  end
+  def two_pairs
+    counts = Hash.new(0)
+    @sorted_values.each do |item|
+      counts[item] += 1
+    end
+    counts.each do |key, value|
+      return true if (counts[key] == 2 && counts.length == 3)
+    end
+    false
+  end
+  def pair
+    counts = Hash.new(0)
+    @sorted_values.each do |item|
+      counts[item] += 1
+    end
+    counts.each do |key, value|
+      return true if counts[key] == 2
+    end
+    false
+  end
+
 
 end
-cards = [
-        Card.new(10, "Hearts"),
-        Card.new(11, "Hearts"),
-        Card.new(12, "Hearts"),
-        Card.new(13, "Hearts"),
-        Card.new(1, "Hearts")
-      ]
